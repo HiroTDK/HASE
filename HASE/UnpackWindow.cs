f39a5f4d956ad01d97e2e849de8e300530bcd0c5
HiroTDK <Necrolord.Xeno@GMail.com>
2017-02-13 01:22:49 -0500

    Got the unpacker working properly.

--- HASE/UnpackWindow.cs
+++ HASE/UnpackWindow.cs
@@ -18,10 +18,6 @@ namespace HASE
 			InitializeComponent();
 		}
 
-		private string file;
-		private string path;
-		private string folder;
-
 		private void checkLabel_Click(object sender, EventArgs e)
 		{
 			Label l = (Label)sender;
@@ -203,32 +199,74 @@ namespace HASE
 			}
 		}
 
+/**************************************************************************************************\
+
+This section covers file and path selection, as well as handling errors and exceptions involving
+selection, as well as starting the unpacking process and catching any exceptions thrown therein.
+
+			button1_Click() is accessed by clicking both label2 and button1.
+			button2_Click() is accessed by clicking both label4 and button2.
+			button3_Click() is accessed by clicking button3.
+ 
+\**************************************************************************************************/
+
+		/// <summary>
+		/// The file path of the ROM that will be unpacked.
+		/// </summary>
+		private string file;
+
+		/// <summary>
+		/// The location of the new folder the ROM will be unpacked into.
+		/// </summary>
+		private string path;
+
+		/// <summary>
+		/// The name of the new folder.
+		/// Name is in the format, "[Code] Title", as defined in the ROM header.
+		/// </summary>
+		private string folder;
+
+
+		/// <summary>
+		/// Handles ROM selection and folder naming, and handles any errors encountered.
+		/// <para>Called through the button1.Click or label2.Click EventHandler.</para>
+		/// </summary>
+		/// <param name="sender">button1 -or- label2</param>
+		/// <param name="e">Unused in this function.</param>
 		private void button1_Click(object sender, EventArgs e)
 		{
-			using (OpenFileDialog open = new OpenFileDialog())
+			// This will be our dialog for choosing a ROM.
+			using (OpenFileDialog openFileDialog = new OpenFileDialog())
 			{
-				open.Title = "Select a ROM to unpack.";
-				open.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-				open.RestoreDirectory = true;
-				open.Filter = "NDS ROM (*.nds)|*.nds";
-				DialogResult result = open.ShowDialog();
+				openFileDialog.Title = "Select a ROM to unpack.";
+				openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+				openFileDialog.RestoreDirectory = true;
+				openFileDialog.Filter = "NDS ROM (*.nds)|*.nds";        //Only NDS files so far. ZIP, RAR, and 7Z may come later.
+				DialogResult result = openFileDialog.ShowDialog();
 
+				// Any result other than selecting a file and hitting [Open] will cancel this function.
 				if (result != DialogResult.OK)
 				{
 					return;
 				}
 
+				// Array of bytes for reading the ROM file.
 				byte[] bytes;
 
-				if (open.CheckFileExists)
+				// If the selected file exists ...
+				if (openFileDialog.CheckFileExists)
 				{
-					bytes = File.ReadAllBytes(open.FileName);
+					// ... then copy the entirety of the ROM to the byte array.
+					bytes = File.ReadAllBytes(openFileDialog.FileName);
 				}
+				// If the selected file doesn't exist ...
 				else
 				{
+					// ... then end the function.
 					return;
 				}
 
+				// If the file isn't long enough to define a header ...
 				if (bytes.Length < 136)
 				{
 					result = CustomMessageBox.Show(
@@ -241,68 +279,91 @@ namespace HASE
 						new List<string>(),
 						new List<DialogResult>());
 
+					// ... then we get a nice message about it and then end the function.
 					return;
 				}
 
+				// We create a MemoryStream from the byte array to allow us to read data from the ROM.
 				using (MemoryStream memoryStream = new MemoryStream(bytes))
 				{
-					using (BinaryReader binaryReader = new BinaryReader(memoryStream))
+					// Now we repurpose the bytes array to read the header size.
+					bytes = new byte[4];
+					memoryStream.Position = 132;
+					memoryStream.Read(bytes, 0, 4);
+					int headerSize = BitConverter.ToInt32(bytes, 0);
+
+					// If the header size is non-standard, i.e., wrong ...
+					if (headerSize != 16384)
 					{
-						bytes = binaryReader.ReadBytes(16);
-						folder = "[" + System.Text.Encoding.UTF8.GetString(bytes, 12, 4);
-						folder += "] " + System.Text.Encoding.UTF8.GetString(bytes, 0, 10);
+						result = CustomMessageBox.Show(
+							"Header Size Error",
+							"The header size is non-standard.",
+							"The 4 bytes at 132 (0x84) indicate that the header size is "
+							+ headerSize + " (0x" + headerSize.ToString("X") + ") bytes long."
+							+ " All known headers are 16384 (0x4000) bytes long,"
+							+ " The header size is either incorrect or the ROM is corrupted."
+							+ " Would you like to proceed anyway?",
+							365, 225,
+							new List<string>() { "Yes", "No" },
+							new List<DialogResult>() { DialogResult.Yes, DialogResult.No });
 
-						bytes = new byte[4];
-						memoryStream.Position = 132;
-						memoryStream.Read(bytes, 0, 4);
-						int headerSize = BitConverter.ToInt32(bytes, 0);
-
-						if (headerSize != 16384)
+						// ... then we get a nice message about it and it asks if we want to continue. Anything but Yes ends the function.
+						if (result != DialogResult.Yes)
 						{
-							result = CustomMessageBox.Show(
-								"Header Size Error",
-								"The header size is non-standard.",
-								"The 4 bytes at 132 (0x84) indicate that the header size is "
-								+ headerSize + " (0x" + headerSize.ToString("X") + ") bytes long."
-								+ " All known headers are 16384 (0x4000) bytes long,"
-								+ " The header size is either incorrect or the ROM is corrupted."
-								+ " Would you like to proceed anyway?",
-								365, 225,
-								new List<string>() { "Yes", "No" },
-								new List<DialogResult>() { DialogResult.Yes, DialogResult.No });
-
-							if (result != DialogResult.Yes)
-							{
-								return;
-							}
-						}
-
-						if (memoryStream.Length < headerSize)
-						{
-							result = CustomMessageBox.Show(
-								"Header Size Error",
-								"The file is too short.",
-								"The header size defined at 132 (0x84) indicates a header size of "
-								+ headerSize + "(0x" + headerSize.ToString("X") + ") bytes."
-								+ "This file is only " + memoryStream.Length + " (0x" + memoryStream.Length.ToString("X") + ") bytes long.",
-								365, 225,
-								new List<string>() { },
-								new List<DialogResult>() { });
-
 							return;
 						}
 					}
+
+					// If the file isn't even as long as the header is supposed to be ...
+					if (memoryStream.Length < headerSize)
+					{
+						result = CustomMessageBox.Show(
+							"Header Size Error",
+							"The file is too short.",
+							"The header size defined at 132 (0x84) indicates a header size of "
+							+ headerSize + "(0x" + headerSize.ToString("X") + ") bytes."
+							+ "This file is only " + memoryStream.Length + " (0x" + memoryStream.Length.ToString("X") + ") bytes long.",
+							365, 225,
+							new List<string>() { },
+							new List<DialogResult>() { });
+
+						// ... then we get a nice message about it and end the function.
+						return;
+					}
+
+					// Now we repurpose the bytes array, resizing it and filling it with the first 16 bytes of the ROM.
+					bytes = new byte[16];
+					memoryStream.Position = 0;
+					memoryStream.Read(bytes, 0, 16);
+
+					// We use this data now to put together the name of the folder we'll be unpacking the ROM into.
+					// The GameCode is the last 4 bytes in UTF8.
+					folder = "[" + System.Text.Encoding.UTF8.GetString(bytes, 12, 4);
+					// The GameTitle is the first 10 bytes in UTF8.
+					folder += "] " + System.Text.Encoding.UTF8.GetString(bytes, 0, 10);
+
 				}
 
-				this.file = open.FileName;
-				this.label2.Text = open.SafeFileName;
+				// If everything checks out, we lock in the file and display the filename on label2.
+				this.file = openFileDialog.FileName;
+				this.label2.Text = openFileDialog.SafeFileName;
+
+				// Then we enable button2 and label4.
 				this.button2.Enabled = true;
 				this.label4.Enabled = true;
 			}
 		}
+		
 
+		/// <summary>
+		/// Handles folder selection, folder conflicts, and handles any errors encountered.
+		/// <para>Called through the button2.Click or label4.Click EventHandler.</para>
+		/// </summary>
+		/// <param name="sender">button2 -or- label4</param>
+		/// <param name="e">Unused in this function.</param>
 		private void button2_Click(object sender, EventArgs e)
 		{
+			// If for some reason this function is called before a file has been selected ...
 			if (file == null || file.Length == 0)
 			{
 				DialogResult result = CustomMessageBox.Show(
@@ -316,9 +377,11 @@ namespace HASE
 					new List<string>(),
 					new List<DialogResult>());
 
+				// ... then we get a nice message about it and end the function.
 				return;
 			}
 
+			// This will be our dialog for choosing a location for our output folder.
 			using (FolderBrowserDialog browser = new FolderBrowserDialog())
 			{
 				browser.Description = "Select a location to unpack the ROM."
@@ -328,87 +391,28 @@ namespace HASE
 				browser.RootFolder = Environment.SpecialFolder.Desktop;
 				DialogResult result = browser.ShowDialog();
 
+				// Anything other than choosing a location and hittng [OK] ends the function.
 				if(result != DialogResult.OK)
 				{
 					return;
 				}
 
+				// Set path to the selcted path from the dialog.
 				path = browser.SelectedPath;
 
-				if (Path.GetFileName(path) == folder)
+				// If the selected folder is one previously created by this unpacker ...
+				if (!Path.GetFileName(path).StartsWith(folder))
 				{
-					result = CustomMessageBox.Show(
-						"Folder Already Exists",
-						"The selected folder matches the new folder.",
-						"The selected folder uses the same name that the new directory uses, probably created from a prior unpacking."
-						+ " Would you like to create a new directory of the same name inside this folder and unpack there,"
-						+ " or would you prefer to overwrite the existing folder with a new directory?",
-						365, 250,
-						new List<string>() { "New Directory", "Overwrite" },
-						new List<DialogResult>() { DialogResult.Ignore, DialogResult.Retry });
-
-					if (result == DialogResult.Ignore)
-					{
-						path += browser.SelectedPath + "\\" + folder;
-					}
-					else if (result == DialogResult.Retry)
-					{
-						DialogResult delete = CustomMessageBox.Show(
-							"Delete Folder?",
-							"Permanently delete " + Path.GetFileName(path) + "?",
-							"Are you sure you would like to delete ths folder and its contents?"
-							+ "\n\n" + path + "\n\n"
-							+ "These folder and its files cannot (easily) be recovered.",
-							460, 225,
-							new List<string>() { "Yes", "No" },
-							new List<DialogResult>() { DialogResult.Yes, DialogResult.No });
-
-						if (delete == DialogResult.Yes)
-						{
-							try
-							{
-								Directory.Delete(path, true);
-							}
-							catch (Exception exception)
-							{
-								string[] titles =
-								{
-									"What have you done?",
-									"You realy stepped in it now.",
-									"YOU BLEW IT UP!",
-									"I can't believe you've done this.",
-									"How did you manage this?",
-									"You're literally the worst."
-								};
-
-								DialogResult dialog = CustomMessageBox.Show(
-									titles[new Random().Next(titles.Count())],
-									exception.GetType().ToString(),
-									exception.Message,
-									365, 155,
-									new List<string>(),
-									new List<DialogResult>());
-
-								return;
-							}
-						}
-						else
-						{
-							return;
-						}
-					}
-					else if (result == DialogResult.Cancel)
-					{
-						return;
-					}
+					// ... then append the folder name to the path to give us our new directory path.
+					path += "\\" + folder;
+					System.Console.WriteLine(path);
 				}
-				else
-				{
-					path += browser.SelectedPath + "\\" + folder;
-				}
+
+				// If the folder already exists ... 
 
 				if (Directory.Exists(path))
 				{
+					// ... then run a loop to find out if and how many copies exist.
 					int i = 2;
 					while (true)
 					{
@@ -422,6 +426,7 @@ namespace HASE
 						}
 					}
 
+					// Ask if we'd like to create a new directory with a number appended, or overwrite the original.
 					result = CustomMessageBox.Show(
 						"Directory Already Exists",
 						"Folder already contains directory.",
@@ -459,10 +464,9 @@ namespace HASE
 								string[] titles =
 								{
 									"What have you done?",
-									"You realy stepped in it now.",
+									"You realy stepped in it this time.",
 									"YOU BLEW IT UP!",
 									"I can't believe you've done this.",
-									"How did you manage this?",
 									"You're literally the worst."
 								};
 
@@ -470,7 +474,7 @@ namespace HASE
 									titles[new Random().Next(titles.Count())],
 									exception.GetType().ToString(),
 									exception.Message,
-									365, 155,
+									450, 250,
 									new List<string>(),
 									new List<DialogResult>());
 
@@ -524,19 +528,19 @@ namespace HASE
 
 				return;
 			}
-
+			/*
 			try
 			{
+				new Unpacker(file, path, false);
 			}
 			catch (Exception exception)
 			{
 				string[] titles =
 				{
 					"What have you done?",
-					"You realy stepped in it now.",
+					"You realy stepped in it this time.",
 					"YOU BLEW IT UP!",
 					"I can't believe you've done this.",
-					"How did you manage this?",
 					"You're literally the worst."
 				};
 
@@ -550,6 +554,10 @@ namespace HASE
 
 				return;
 			}
+			*/
+			Control c = (Control)sender;
+			c.Enabled = false;
+			new Unpacker(file, path, false);
 		}
 	}
 }
